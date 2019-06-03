9f50047557491f6e1ff72b1eda21cc5229048c92
CHENJIE-PC\Administrator <530216775@qq.com>
2019-06-03 22:00:20 +0800

    6.0

---
+++ Csharp6.0/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp6._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        #region 只读自动属性
+        public string FirstName { get; }
+        public string LastName { get; }
+        #endregion
+        #region 自动属性初始化表达式
+
+
+
+        #endregion
+        #region Expression-bodied函数成员
+
+
+
+        #endregion
+        #region using static
+
+
+
+        #endregion
+        #region Null条件运算符
+
+
+
+        #endregion
+        #region 字符串内插
+
+
+
+        #endregion
+        #region 异常筛选器
+
+
+
+        #endregion
+        #region nameof表达式
+
+
+
+        #endregion
+        #region Catch和Finally块中的Await
+
+
+
+        #endregion
+        #region 使用索引器初始化关联集合
+
+
+
+        #endregion
+        #region 集合初始值设定项中的扩展Add方法
+
+
+
+        #endregion
+
+        #region 改进了重载解析
+
+
+
+        #endregion
+    }
+}
